c15c255e37055021478b2682426abddf3e3458d7
M-a-y-u-r <115573684+M-a-y-u-r@users.noreply.github.com>
2023-12-27 20:38:49 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,49 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ranjha",
+    artist: "B. Praak · Jasleen Royal",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "O Maahi",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Lutt Putt Gaya",
+    artist: "Arijit Singh",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Agar Tum Saath Ho",
+    artist: "Alka Yagnik, Arijit Singh",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Nikle The Kabhi Hum Ghar Se",
+    artist: "Sonu Nigam",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Humsafar",
+    artist: "Akhil Sachdeva, Mansheel Gujral",
+    img: "music-6",
+    src: "music-6",
+  },
+];
